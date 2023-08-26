5633734cfd2cf462daec3c62d206ce7e0e529da0
TheFattal <94184930+TheFattal@users.noreply.github.com>
2023-08-26 20:50:13 +0300

    Update The_Record_Collection.sh

--- The_Record_Collection.sh
+++ The_Record_Collection.sh
@@ -21,10 +21,10 @@ fi
 
 #Validation-3 --- Checks if the file already exists - and creates log file if necessary:
 if [[ -e $file_name ]] ; then
-    echo "File '$file_name' already exists - let's use it! "
+    echo "The DB file '$file_name' already exists - let's use it! "
     declare -g log_file_name="${file_name}_log"
     if [ -e "$log_file_name" ]; then
-       echo "And it's log file '$log_file_name' exists too."
+       echo "And it's log file '$log_file_name' exists too!"
     else
        touch "$log_file_name"
        echo "A log file '$log_file_name' was successfull created"
@@ -44,7 +44,7 @@ touch "$file_name"
 declare -g log_file_name="${file_name}_log"
 
 touch "$log_file_name"
-echo "DB File '$file_name' and log file '$file_name_log' were successfully created."
+echo "DB File '$file_name' and log file '$log_file_name' were successfully created."
 }
 
 #This function searches the Data-Base by record name:
@@ -56,13 +56,13 @@ Search_Record()
 	done < <(grep -i "$recordName" "$file")
 	if [ "${#lines[@]}" -eq 0 ]; then
 		echo "No lines containing '$recordName' found in the file."
-		echo "$(date) Search Failure" >> log.txt
+		echo "$(date) Search Failure" >> "$log_file_name"
         	return 0
    	fi
    	for element in "${lines[@]}"; do
     		echo "$element"
 	done
-	echo "$(date) Search Success" >> log.txt
+	echo "$(date) Search Success" >> "$log_file_name"
 }
 
 #This function prints all the Data-Base sorted: 
@@ -70,39 +70,43 @@ Print_Sorted()   {
    
    if [[ -s "$file" ]]; then
        sort -k1,1 "$file"
-       echo "$(date) PrintAll Success" >> log.txt
+       echo "$(date) PrintAll Success" >> "$log_file_name" 
+ 
+       # Writes all the Sorted Records to the log file:
+       sorted_file=$(sort -k1,1 "$file")
+       date_prefix=$(date)
+       modified_text=$(echo "$sorted_file" | awk -v date="$date_prefix" '{print date, $0}')
+       echo "$modified_text" >> "$log_file_name"
+
    else 
        echo "No records in the DataBase!"
-       echo "$(date) PrintAll Failure" >> log.txt
+       echo "$(date) PrintAll Failure" >> "$log_file_name"
    fi
    
 }
 
 #The function calculates the TOTAL amount of records in the Data-Base:
 Print_Total_Amount() 
-{
-    # Check if the records file exists
-    if [ ! -f "$file" ]; then
-        echo "No records file found."
-        echo "Task completed."
-        echo "$(date) PrintAmount Failure" >> log.txt
-        return 0
-    fi
-    
+{   
     #Calculets total amount - and print the outcome:
     total_quantity=$(awk '{n += $NF}; END{print n}' $file)
     if [[ total_quantity -gt 0 ]]; then
         echo "The records TOTAL AMOUNT is: $total_quantity"
-        echo "$(date) PrintAmount Success" >> log.txt
+        echo "$(date) PrintAmount Success" >> "$log_file_name"
+        echo "$(date) PrintAmount $total_quantity" >> "$log_file_name"
     else
         echo "The are NO records in the Data-Base!"
+        echo "$(date) PrintAmount Success" >> "$log_file_name"
+        echo "$(date) PrintAmount $total_quantity" >> "$log_file_name"
      fi   
 }
 
 
 Search_by_name()
 { 
-        lines=0
+        for ((i=0; i<${#lines[@]}; i++)); do
+        my_array[$i]=0
+        done
 	recordName="$1"
 	while IFS= read -r line; do
     		lines+=("$line")
@@ -142,8 +146,10 @@ Update_Name()
         	IFS=',' read -ra oldName <<< "$chosenLine"
         	sed -i "s/^${oldName[0]},/${newName},/" "$file"
        		echo "Replacement done in $file"
+       		echo "$(date) UpdateName Success" >> "$log_file_name"
 	else
 		echo invalid option
+		echo "$(date) UpdateName Failure" >> "$log_file_name"
 		return 0
 	fi
 }
@@ -161,8 +167,10 @@ Update_Amount()
 		# Replace the line with the new number using sed
 		sed -i "s/^$escaped_line/${chosenLine%,*}, $quantity/" "$file"
 		echo "Replacement done in $file"
+		echo "$(date) UpdateName Success" >> "$log_file_name"
 	else
 		echo invalid option
+		echo "$(date) UpdateAmount Failure" >> "$log_file_name"
 		return 0
 	fi
 	
@@ -175,13 +183,17 @@ local record_name
 read -p "Please enter a new record name you would like to add: " record_name
 read -p "Please enter the record's amount: " record_amount
 #Search for resaults and show the selections in a menu:
-chosenLine=0
+
 Search_by_name $record_name
 find_lines_length=${#lines[@]}
 if [[ find_lines_length -ge 1 ]]; then
-echo "lines number are $find_lines_length"
 
 lines+=("ADD THE SEARCH: $record_name, $record_amount")
+echo "Typing lines:"
+for item in "${lines[@]}"; do
+    echo "$item"
+done
+
 PS3="Select a line: "
 	select selected_line in "${lines[@]}"; do
 	  selected_number=$REPLY
@@ -191,14 +203,16 @@ PS3="Select a line: "
                	break
          else
                	echo "Invalid selection. Please choose a valid line."
+               	echo "$(date) Insert Failure" >> "$log_file_name"
                	chosenLine=""
         fi
        	done
-       	# The case of Adding the current search to the DB, instead od updating quantity:
+       	# The case of Adding the current search to the DB, instead of updating quantity:
        	desired_value=$(( $find_lines_length + 1 ))
        	if [ $REPLY -eq $desired_value ]; then
        	  echo "$record_name, $record_amount" >> $file
 	  echo "A new record was made successfuly in the Data-base!"
+	  echo "$(date) Insert Success" >> "$log_file_name"
 	  return 0
        	fi
              		
@@ -207,15 +221,15 @@ PS3="Select a line: "
 	# Replace the line with the new number using sed
 	sed -i "s/^$escaped_line/${chosenLine%,*}, $record_amount/" "$file"
 	echo "Amount replacement was done in $file"
+	echo "$(date) UpdateAmount Success" >> "$log_file_name"
 	
 #If there were NO search resault at all, add the new record to the DB:		
 else 
   	       echo "$record_name, $record_amount" >> $file
-	       echo "A new record was made successfuly in the Data-base!"	       
+	       echo "A new record was made successfuly in the Data-base!"
+	       echo "$(date) Insert Success" >> "$log_file_name"	       
 
 fi
-
-
 }
 
 #This function removes a record to the file:
@@ -305,6 +319,7 @@ done
 #Input Arguments:
 file="$1"
 file2="$2"
-#Call Create_File function, and then running the menu:
+#Call Create_Files function:
 Create_Files "$file" "$file2"
+#Running the Menu:
 Menu
